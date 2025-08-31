412a9293c92514507b0cf340efd5c83d8235a630
Wafiq <143102252+Wafee17@users.noreply.github.com>
2025-08-31 20:32:45 +0530

    Update onlinevotingsystem.sql

--- assets/onlinevotingsystem.sql
+++ assets/onlinevotingsystem.sql
@@ -1,25 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Aug 31, 2025 at 04:49 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Online Voting System Database Schema
+-- This file contains only the table structure without any sample data
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `onlinevotingsystem`
---
 
 -- --------------------------------------------------------
 
@@ -28,24 +12,15 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `candidate_details` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `election_id` int(11) DEFAULT NULL,
   `candidate_name` varchar(255) DEFAULT NULL,
   `party_details` text DEFAULT NULL,
   `candidate_photo` text DEFAULT NULL,
   `inserted_by` varchar(255) DEFAULT NULL,
-  `inserted_on` date DEFAULT NULL
+  `inserted_on` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `candidate_details`
---
-
-INSERT INTO `candidate_details` (`id`, `election_id`, `candidate_name`, `party_details`, `candidate_photo`, `inserted_by`, `inserted_on`) VALUES
-(15, 5, 'David', '----', '../assets/images/candidate_photos/1380177609_7052312420candidate1.jpg', 'Wafiq', '2025-02-02'),
-(16, 5, 'Michael', '----', '../assets/images/candidate_photos/3088256795_1414457460candidate2.jpg', 'Wafiq', '2025-02-02'),
-(17, 5, 'George', '----', '../assets/images/candidate_photos/8121136008_6484039376candidate3.jpg', 'Wafiq', '2025-02-02'),
-(18, 5, 'Mark', '----', '../assets/images/candidate_photos/3324242687_4944490838images.jpeg', 'Wafiq', '2025-02-02');
 
 -- --------------------------------------------------------
 
@@ -54,22 +29,16 @@ INSERT INTO `candidate_details` (`id`, `election_id`, `candidate_name`, `party_d
 --
 
 CREATE TABLE `elections` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `election_topic` varchar(255) DEFAULT NULL,
   `no_of_candidates` int(11) DEFAULT NULL,
   `starting_date` date DEFAULT NULL,
   `ending_date` date DEFAULT NULL,
   `status` varchar(45) DEFAULT NULL,
   `inserted_by` varchar(255) DEFAULT NULL,
-  `inserted_on` date DEFAULT NULL
+  `inserted_on` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `elections`
---
-
-INSERT INTO `elections` (`id`, `election_topic`, `no_of_candidates`, `starting_date`, `ending_date`, `status`, `inserted_by`, `inserted_on`) VALUES
-(5, 'Local Election', 8, '2025-05-08', '2025-05-12', 'Expired', 'Wafiq', '2025-08-31');
 
 -- --------------------------------------------------------
 
@@ -78,24 +47,15 @@ INSERT INTO `elections` (`id`, `election_topic`, `no_of_candidates`, `starting_d
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) DEFAULT NULL,
   `email` varchar(45) DEFAULT NULL,
   `mobile_number` varchar(45) DEFAULT NULL,
   `password` text DEFAULT NULL,
-  `user_role` varchar(45) DEFAULT NULL,
-  `otp` varchar(45) DEFAULT NULL
+  `user_role` varchar(45) DEFAULT 'Voter',
+  `otp` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `username`, `email`, `mobile_number`, `password`, `user_role`, `otp`) VALUES
-(11, 'Wafiq', 'mohammadwafiq58@gmail.com', '8660308953', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', 'Admin', NULL),
-(18, 'David', 'musicholic177@gmail.com', '8598378796', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', 'Voter', NULL),
-(19, 'Kshithij Rai K', 'kshithijraik@gmail.com', '8867265752', '5150d2104c8cd974b27fad3f25ec4e8098bb7bbe', 'Voter', NULL),
-(20, 'Manish Shetty', 'manishshetty071@gmail.com', '6361196763', '6216f8a75fd5bb3d5f22b6f9958cdede3fc086c2', 'Voter', NULL);
 
 -- --------------------------------------------------------
 
@@ -104,78 +64,13 @@ INSERT INTO `users` (`id`, `username`, `email`, `mobile_number`, `password`, `us
 --
 
 CREATE TABLE `votings` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `election_id` int(11) DEFAULT NULL,
   `voters_id` int(11) DEFAULT NULL,
   `candidate_id` int(11) NOT NULL,
   `vote_date` date DEFAULT NULL,
-  `vote_time` time DEFAULT NULL
+  `vote_time` time DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `votings`
---
-
-INSERT INTO `votings` (`id`, `election_id`, `voters_id`, `candidate_id`, `vote_date`, `vote_time`) VALUES
-(11, 5, 18, 15, '2025-02-02', '12:06:18');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `candidate_details`
---
-ALTER TABLE `candidate_details`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `elections`
---
-ALTER TABLE `elections`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `votings`
---
-ALTER TABLE `votings`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `candidate_details`
---
-ALTER TABLE `candidate_details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
-
---
--- AUTO_INCREMENT for table `elections`
---
-ALTER TABLE `elections`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
-
---
--- AUTO_INCREMENT for table `votings`
---
-ALTER TABLE `votings`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
